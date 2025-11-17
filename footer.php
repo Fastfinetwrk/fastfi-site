d1502da87e5a1287b6ae6d8c44774d547139b30d
Fastfinetwrk <rathodvishal502@gmail.com>
2025-11-17 15:07:20 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,10 +1,47 @@
-<footer class="footer-admin mt-auto footer-light">
-                    <div class="container-xl px-4">
-                        <div class="row">
-                   
-                            <div class="col-md-6 text-md-end small">
-                               <p>Internet Service Provider Management System</p>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+<!-- footer -->
+<div class="footer">
+    <div class="agile_footer_grids">
+        <!-- Contact Info -->
+        <div class="col-md-6 col-sm-6 col-xs-6 w3_footer_grid agile_footer_grids_w3_footer">
+            <div class="w3_footer_grid_bottom contact">
+                <h3>Contact Info</h3>
+                <ul>
+                    <li>
+                        <span class="fa fa-map-marker" aria-hidden="true"></span>
+                        <p>Fast-FI Office, Ayodhya Nagar, Bharuch, Gujarat – 392001, India</p>
+                    </li>
+                    <li>
+                        <span class="fa fa-envelope-o" aria-hidden="true"></span>
+                        <a href="mailto:support@fastfi.in">support@fastfi.in</a>
+                    </li>
+                    <li>
+                        <span class="fa fa-phone" aria-hidden="true"></span>
+                        <p>+91 91064 07995</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Quick Links -->
+        <div class="col-md-6 col-sm-6 col-xs-6 w3_footer_grid agile_footer_grids_w3_footer">
+            <div class="w3_footer_grid_bottom contact">
+                <h3>Quick Links</h3>
+                <ul class="w3_footer_grid_list">
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="bbplan.php">Broadband Plans</a></li>
+                    <li><a href="dthplan.php">Digital Cable TV</a></li>
+                    <li><a href="admin/login.php">Admin Login</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="clearfix"></div>
+    </div>
+
+    <!-- Copyright -->
+    <div class="wthree_footer_copy">
+        <p>© <?php echo date("Y"); ?> Fast-FI — Bharuch’s Local, World-Class Broadband. All Rights Reserved.</p>
+    </div>
+</div>
+<!-- //footer -->
--- header.php
+++ header.php
@@ -1,152 +1,254 @@
-<nav class="topnav navbar navbar-expand shadow justify-content-between justify-content-sm-start navbar-light bg-white" id="sidenavAccordion">
-    
-    <!-- Sidenav Toggle Button -->
-    <button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 me-2 ms-lg-2 me-lg-0" id="sidebarToggle">
-        <i data-feather="menu"></i>
-    </button>
+<?php 
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
 
-<a class="navbar-brand pe-3 ps-4 ps-lg-3" href="dashboard.php">
-    <span class="d-inline-flex align-items-center">
-        <span class="fastfi-brand-text" style="font-size:22px; font-weight:700; color:#0056D6;">fast-FI</span>
-    </span>
-</a>
+if(isset($_POST['signup']))
+{
+    $fname = $_POST['fname'];
+    $mobno = $_POST['mobno'];
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
 
+    $ret = "SELECT Email FROM tbluser WHERE Email = :email";
+    $query = $dbh->prepare($ret);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->execute();
 
+    if($query->rowCount() == 0)
+    {
+        $sql = "INSERT INTO tbluser(FullName,MobileNumber,Email,Password) VALUES(:fname,:mobno,:email,:password)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':fname', $fname, PDO::PARAM_STR);
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
+        $query->bindParam(':mobno', $mobno, PDO::PARAM_STR);
+        $query->bindParam(':password', $password, PDO::PARAM_STR);
+        $query->execute();
+        $lastInsertId = $dbh->lastInsertId();
+        if($lastInsertId)
+        {
+            echo "<script>alert('You have signed up successfully');</script>";
+        }
+        else
+        {
+            echo "<script>alert('Something went wrong. Please try again');</script>";
+        }
+    }
+    else
+    {
+        echo "<script>alert('Email-id already exists. Please try again');</script>";
+    }
+}
 
+if(isset($_POST['login'])) 
+{
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+    $sql = "SELECT ID FROM tbluser WHERE Email=:email AND Password=:password";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->execute();
+    if($query->rowCount() > 0)
+    {
+        $result = $query->fetch(PDO::FETCH_OBJ);
+        $_SESSION['ispmsuid'] = $result->ID;
+        $_SESSION['login'] = $email;
+        echo "<script type='text/javascript'> document.location ='index.php'; </script>";
+    } 
+    else
+    {
+        echo "<script>alert('Invalid Details');</script>";
+    }
+}
+?>
 
-    <!-- Navbar Items -->
-    <ul class="navbar-nav align-items-center ms-auto">
+<div class="header">
+    <div class="container-fluid">
+        <div class="header-grid">
+            <div class="header-grid-left">
+                <ul>
+                    <?php
+                    $sql = "SELECT * FROM tblpage WHERE PageType='contactus'";
+                    $query = $dbh->prepare($sql);
+                    $query->execute();
+                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                    if($query->rowCount() > 0)
+                    {
+                        foreach($results as $row)
+                        {
+                            ?>
+                            <li>
+    <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
+    <a href="mailto:support@fastfi.in">support@fastfi.in</a>
+</li>
 
-        <!-- Broadband Alerts -->
-        <li class="nav-item dropdown no-caret d-none d-sm-block me-3 dropdown-notifications">
-            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownAlerts" href="#" role="button" data-bs-toggle="dropdown">
-                <i data-feather="bell"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownAlerts">
-                <?php
-                $sql="SELECT * from tblbookbplan where Status is null";
-                $query = $dbh->prepare($sql);
-                $query->execute();
-                $results=$query->fetchAll(PDO::FETCH_OBJ);
-                $totnew1=$query->rowCount();
-                ?>
-                <h6 class="dropdown-header dropdown-notifications-header">
-                    <i class="me-2" data-feather="bell"></i>
-                    Broadband Notification (<?php echo $totnew1;?>)
-                </h6>
-                <?php foreach($results as $row){ ?>
-                <a class="dropdown-item dropdown-notifications-item" href="new-request.php">
-                    <div class="dropdown-notifications-item-icon bg-warning">
-                        <i data-feather="activity"></i>
-                    </div>
-                    <div class="dropdown-notifications-item-content">
-                        <div class="dropdown-notifications-item-content-details"><?php echo $row->BookingDate;?></div>
-                        <div class="dropdown-notifications-item-content-text"><?php echo $row->BookingNumber;?>.</div>
-                    </div>
-                </a>
-                <?php } ?>
-                <a class="dropdown-item dropdown-notifications-footer" href="all-request.php">View All Notification</a>
+<li>
+    <i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>
+    +91 9106407995
+</li>
+
+                            <?php
+                        }
+                    }
+                    ?>
+
+                    <?php if (empty($_SESSION['ispmsuid'])) { ?>
+                        <li><i class="glyphicon glyphicon-log-in" aria-hidden="true"></i>
+                            <a href="#" class="login" data-toggle="modal" data-target="#myModal4">Login</a></li>
+                        <li><i class="glyphicon glyphicon-book" aria-hidden="true"></i>
+                            <a href="#" class="login reg" data-toggle="modal" data-target="#myModal5">Register</a></li>
+                    <?php } ?>
+                </ul>
             </div>
-        </li>
+            <div class="clearfix"> </div>
+        </div>
 
-        <!-- DTH Alerts -->
-        <li class="nav-item dropdown no-caret d-none d-sm-block me-3 dropdown-notifications">
-            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                <i data-feather="bell"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up">
-                <?php
-                $sql2="SELECT * from tblbookdthplan where Status is null";
-                $query2 = $dbh->prepare($sql2);
-                $query2->execute();
-                $results2=$query2->fetchAll(PDO::FETCH_OBJ);
-                $totnew2=$query2->rowCount();
-                ?>
-                <h6 class="dropdown-header dropdown-notifications-header">
-                    <i class="me-2" data-feather="bell"></i>
-                    DTH Notification (<?php echo $totnew2;?>)
-                </h6>
-                <?php foreach($results2 as $row2){ ?>
-                <a class="dropdown-item dropdown-notifications-item" href="new-dthrequest.php">
-                    <div class="dropdown-notifications-item-icon bg-warning">
-                        <i data-feather="activity"></i>
-                    </div>
-                    <div class="dropdown-notifications-item-content">
-                        <div class="dropdown-notifications-item-content-details"><?php echo $row2->BookingDate;?></div>
-                        <div class="dropdown-notifications-item-content-text"><?php echo $row2->BookingNumber;?>.</div>
-                    </div>
-                </a>
-                <?php } ?>
-                <a class="dropdown-item dropdown-notifications-footer" href="all-request.php">View All Notification</a>
+        <div class="logo-nav">
+            <div class="logo-nav-left">
+                <!-- Logo: place your logo file images/fast-fi.png in the images folder -->
+                <h1>
+    <a href="index.php">
+        <img src="images/fast-fi.png" alt="Fast-FI Logo" style="max-height:70px; display:block; margin:5px 0;">
+    </a>
+</h1>
+
             </div>
-        </li>
 
-        <!-- Messages -->
-        <li class="nav-item dropdown no-caret d-none d-sm-block me-3 dropdown-notifications">
-            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                <i data-feather="mail"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up">
-                <?php
-                $sql1="SELECT * from tblcontact where IsRead is null";
-                $query1 = $dbh->prepare($sql1);
-                $query1->execute();
-                $results1=$query1->fetchAll(PDO::FETCH_OBJ);
-                $newmsg=$query1->rowCount();
-                ?>
-                <h6 class="dropdown-header dropdown-notifications-header">
-                    <i class="me-2" data-feather="mail"></i>
-                    Reported Issues (<?php echo $newmsg;?>)
-                </h6>
-                <?php foreach($results1 as $row1){ ?>
-                <a class="dropdown-item dropdown-notifications-item" href="request.php">
-                    <img class="dropdown-notifications-item-img" src="assets/img/illustrations/profiles/profile-2.png" />
-                    <div class="dropdown-notifications-item-content">
-                        <div class="dropdown-notifications-item-content-text"><?php echo $row1->Message;?>.</div>
-                        <div class="dropdown-notifications-item-content-details"><?php echo $row1->Name;?> (<?php echo $row1->Enquirydate;?>)</div>
+            <div class="logo-nav-left1">
+                <nav class="navbar navbar-default">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+                            Menu
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
                     </div>
-                </a>
-                <?php } ?>
-                <a class="dropdown-item dropdown-notifications-footer" href="#!">Read All Messages</a>
-            </div>
-        </li>
 
-        <!-- User Profile -->
-        <li class="nav-item dropdown no-caret dropdown-user me-3 me-lg-4">
-            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                <img class="img-fluid" src="assets/img/illustrations/profiles/profile-1.png" />
-            </a>
-            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up">
-                <h6 class="dropdown-header d-flex align-items-center">
-                    <img class="dropdown-user-img" src="assets/img/illustrations/profiles/profile-1.png" />
-                    <div class="dropdown-user-details">
-                        <?php
-                        $aid=$_SESSION['ispmsaid'];
-                        $sql="SELECT AdminName,Email from tbladmin where ID=:aid";
-                        $query = $dbh->prepare($sql);
-                        $query->bindParam(':aid',$aid,PDO::PARAM_STR);
-                        $query->execute();
-                        $results=$query->fetchAll(PDO::FETCH_OBJ);
-                        if($query->rowCount() > 0){
-                        foreach($results as $row){
-                        ?>
-                        <div class="dropdown-user-details-name"><?php echo $row->AdminName;?></div>
-                        <div class="dropdown-user-details-email"><?php echo $row->Email;?></div>
-                        <?php }} ?>
+                    <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li class="agileits dropdown"><a href="about.php">About</a></li>
+                            <li><a href="bbplan.php">Broadband</a></li>
+                            <li><a href="dthplan.php">Digital Cable TV</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+
+                            <?php if (!empty($_SESSION['ispmsuid'])) { ?>
+                                <li class="agileits dropdown">
+                                    <a href="#" data-toggle="dropdown" aria-expanded="true">My Account</a>
+                                    <ul class="dropdown-menu agile_short_dropdown">
+                                        <li><a href="profile.php">Profile</a></li>
+                                        <li><a href="change-password.php">Change Password</a></li>
+                                        <li><a href="logout.php">Logout</a></li>
+                                    </ul>
+                                </li>
+                                <li class="agileits dropdown">
+                                    <a href="#" data-toggle="dropdown" aria-expanded="true">Request History</a>
+                                    <ul class="dropdown-menu agile_short_dropdown">
+                                        <li><a href="broadband-request.php">Broadband</a></li>
+                                        <li><a href="dth-request.php">Digital Cable TV</a></li>
+                                    </ul>
+                                </li>
+                            <?php } ?>
+
+                            <?php if (empty($_SESSION['ispmsuid'])) { ?>
+                                <li><a href="admin/login.php">Admin</a></li>
+                            <?php } ?>
+                        </ul>
                     </div>
-                </h6>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="profile.php">
-                    <div class="dropdown-item-icon"><i data-feather="settings"></i></div> Profile
-                </a>
-                <a class="dropdown-item" href="change-password.php">
-                    <div class="dropdown-item-icon"><i data-feather="settings"></i></div> Change Password
-                </a>
-                <a class="dropdown-item" href="logout.php">
-                    <div class="dropdown-item-icon"><i data-feather="log-out"></i></div> Logout
-                </a>
+                </nav>
             </div>
-        </li>
 
-    </ul>
-</nav>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+</div>
+<!-- //header -->
+
+<div class="general_social_icons">
+    <nav class="wthree-social">
+        <ul>
+            <li class="w3_facebook"><a href="#"> <i class="fa fa-facebook"></i>Facebook</a></li>
+            <li class="w3_twitter"><a href="#"><i class="fa fa-twitter"></i>Twitter </a></li>
+            <li class="w3_dribbble"><a href="#"> <i class="fa fa-dribbble"></i>Dribbble</a></li>
+            <li class="w3_g_plus"><a href="#"><i class="fa fa-google-plus"></i>Google+ </a></li>
+        </ul>
+    </nav>
+</div>
+
+<!-- Modal: Login -->
+<div class="modal fade" id="myModal4" tabindex="-1" role="dialog" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4>Login</h4>
+                <div class="login-form">
+                    <form method="post" name="login">
+                        <input type="email" name="email" placeholder="E-mail" required="true">
+                        <input type="password" name="password" placeholder="Password" required="true">
+                        <div class="tp">
+                            <input type="submit" name="login" value="LOGIN NOW">
+                        </div>
+                        <div class="forgot-grid">
+                            <div class="forgot">
+                                <a href="forgot-password.php">Forgot Password?</a>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //Modal: Login -->
+
+<!-- Modal: Register -->
+<div class="modal fade" id="myModal5" tabindex="-1" role="dialog" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4>Register</h4>
+                <div class="login-form">
+                    <form method="post" name="signup" onsubmit="return checkpass();">
+                        <input type="text" name="fname" placeholder="Full Name" required="true">
+                        <input type="email" name="email" placeholder="E-mail" required="true">
+                        <input type="text" name="mobno" placeholder="Mobile Number" required="true" maxlength="10" pattern="[0-9]+">
+                        <input type="password" name="password" placeholder="Password" required="true" id="password1">
+                        <!-- fixed name attribute: use confirmpassword (no spaces) -->
+                        <input type="password" name="confirmpassword" placeholder="Confirm Password" required="true" id="password2">
+                        <div class="signin-rit">
+                            <span class="agree-checkbox">
+                                <label class="checkbox">
+                                    <input type="checkbox" name="checkbox" checked required>
+                                    I agree to your <a class="w3layouts-t" href="terms.html">Terms of Use</a> and <a class="w3layouts-t" href="privacy.html" target="_blank">Privacy Policy</a>
+                                </label>
+                            </span>
+                        </div>
+                        <div class="tp">
+                            <input type="submit" name="signup" value="REGISTER NOW">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //Modal: Register -->
+
+<script type="text/javascript">
+    // simple password match check used by onsubmit checkpass()
+    function checkpass(){
+        var p1 = document.getElementById('password1').value;
+        var p2 = document.getElementById('password2').value;
+        if(p1 !== p2){
+            alert('Passwords do not match');
+            return false;
+        }
+        return true;
+    }
+</script>
